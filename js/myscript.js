2b75a0ffb0e5e274d50102dbe68b7aadf949ef1a
elisabichiappini <elisabichiappini@gmail.com>
2025-06-10 17:59:08 +0200

    feat: add index e script

--- js/myscript.js
+++ js/myscript.js
@@ -1,5 +1,19 @@
-let links = [
+let navLinks = [
     {
-        
-    }
+        linkName: "home",
+        title : "home",
+        url: "./index.html"
+    },
+    {
+        linkName: "about",
+        title : "about",
+        url: "./about.html"
+    },
+    {
+        linkName: "contact",
+        title : "contact",
+        url: "./index.html"
+    },
 ]
+
+const ulListLink = document.getElementById('nav-links');
